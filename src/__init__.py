6377ba2700ef5d57b7656a09ea9d2c0519ae4d13
thahmann <torsten@cs.toronto.edu>
2013-03-22 10:22:47 -0400

    Ensure modules in src are loaded properly

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+from ClifModule import *
+from ClifModuleSet import *
+from clif import *
+from commands import *
+from ladr import *
+from filemgt import *
+from process import *
+ 
+__all__ = ["ClifModule", "ClifModuleSet"]
