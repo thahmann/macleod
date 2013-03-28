c1d6bda23cae6855c2dbbd7cf7ee1e6270a6dc3f
thahmann <torsten@cs.toronto.edu>
2013-03-28 13:31:56 -0400

    process.py merged: started use of Reasoner class

--- src/process.py
+++ src/process.py
@@ -53,6 +53,7 @@ def executeSubprocess(command, results = None):
 def terminateSubprocess (process):
 	"""terminate a sub process; needed for downwards compatibility with Python 2.5"""
 	def terminate_win (process):
+		print "Terminating Windows process " + str(process.pid)
 		os.system("taskkill /F /T /PID " + str(process.pid))
 		#value = win32process.TerminateProcess(process._handle, -1)
 #		import win32api
@@ -64,10 +65,13 @@ def terminateSubprocess (process):
 		return value
 
 	def terminate_nix (process):
+		print "Terminating Linux process " + str(process.pid)
 		#os.kill(process.pid, signal.SIGINT)
-		value = os.killpg(process.pid, signal.SIGINT)
-		time.sleep(1.0)
-		return value
+		process.terminate()
+		if process.is_alive():
+			value = os.kill(process.pid, signal.SIGINT)
+			time.sleep(0.5)
+			return value
 
 		#return os.waitpid(process.pid, os.WNOHANG)
 
@@ -88,7 +92,7 @@ def terminateSubprocess (process):
 #		return process.returncode
 
 
-def raceProcesses (provers, modelfinders):
+def raceProcesses (reasoners):
 	"""run a set of theorem provers and a set of model finders in parallel.
 	If one terminates successfully, all others are immediately terminated.
 	
@@ -101,7 +105,7 @@ def raceProcesses (provers, modelfinders):
 	#print provers
 	#print modelfinders
 	
-	proverProcesses = []
+	reasonerProcesses = []
 	
 	# dictionary that give each process a number as name to not have to rely on the command that is altered through serialization
 	#proverDict = {}
@@ -112,17 +116,17 @@ def raceProcesses (provers, modelfinders):
 	results = Queue()
 	
 	# start all processess
-	for prover in (provers.keys() + modelfinders.keys()):
-		#print 'Creating subprocess for ' + prover
-		#proverDict[i] = prover
+	for r in reasoners.get_reasoners():
+		#print 'Creating subprocess for ' + r
+		#proverDict[i] = r
 		# TODO: need a separate class for the execute method in order to gracefully shut it down
-		p = multiprocessing.Process(name=prover.split(' ')[0], target=executeSubprocess, args=(prover,results,))
+		p = multiprocessing.Process(name=r.identifier, target=executeSubprocess, args=(r.get_command(),results,))
 		p.start()
 		time.sleep(0.1)
-		proverProcesses.append(p)
+		reasonerProcesses.append(p)
 		#i += 1
 	
-	num_running = len(proverProcesses)
+	num_running = len(reasonerProcesses)
 	success = False
 	
 	time.sleep(0.1)
@@ -137,49 +141,32 @@ def raceProcesses (provers, modelfinders):
 			(name, code) = results.get()
 			#name = proverDict[name]		# mapping the number back to the real command name
 			#print str(name) + " returned with " + str(code)
-			if name in provers:
-				#print name + " finished; positive returncodes are " + str(provers[name])
-				if code in provers[name]:
-					print name + " found an inconsistency"
-					success = True
-				provers[name] = code
-			if name in modelfinders:
-				#print name + " finished; positive returncodes are " + str(modelfinders[name])
-				if code in modelfinders[name]:
-					print name + " found a model"
-					success = True
-				modelfinders[name] = code
-
+			
+			#print name + " finished; positive returncodes are " + str(provers[name])
+			reasoners[name].return_code = code
+			if code in reasoners[name].positive_returncodes:
+				success = True
+				if reasoners[name].is_prover():
+					print name + " found an proof/inconsistency"
+				else:
+					print name + " found a counterexample/model"
+				
+					
 		if success:
 			# terminate all processes that are still active
 			for p in multiprocessing.active_children():
-#				print "Child process state: %d" % p.is_alive()
-# TODO: want to gracefully shut down				
-#				p.terminate()
+				# TODO: want to gracefully shut down
 				time.sleep(1.0)
-#    			if p.is_alive():
-#	       			print "Child process state: %d" % p.is_alive()
-#       			os.kill(p.pid, signal.SIGTERM)
-#       			time.sleep(1.0)
-#       			if p.is_alive():
-#       				os.kill(p.pid, signal.SIGKILL)
-#       				time.sleep(1.0)
-       			os.system("taskkill /F /T /PID " + str(p.pid))
-		       			#os.system("taskkill /im /f")
-#       			import win32api
-#       			PROCESS_TERMINATE = 1
-#       			handle = win32api.OpenProcess(PROCESS_TERMINATE, False, p.pid())
-#       			win32api.TerminateProcess(handle, -1)
-#       			win32api.CloseHandle(handle)
+				terminateSubprocess(p)
 		break
 
 
-	return (provers, modelfinders)
+	return reasoners
 
 		#num_running = len(active)
 #		print(str(num_running) + " active processses")
 #		time.sleep(0.5)
-#		for p in proverProcesses:
+#		for p in reasonerProcesses:
 #			if p not in active:
 #				print p
 #				if p.returncode in p.provers[p.name()]:
@@ -190,7 +177,7 @@ def raceProcesses (provers, modelfinders):
 #				if p.returncode in p.finders[p.name()]:
 #					success = True
 
-#	for prover in proverProcesses:
-#		provers[prover.name()]=prover.returncode
+#	for r in reasonerProcesses:
+#		provers[r.name()]=r.returncode
 #	for finder in finderProcesses:
 #		modelfinders[finder.name()]=find.returncode
