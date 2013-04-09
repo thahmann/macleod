3395c904c4e41263d000ba8cae7faec00c228910
thahmann <torsten@cs.toronto.edu>
2013-04-09 13:00:39 -0400

    full automation of check_consistency task

--- tasks/check_consistency_all.py
+++ tasks/check_consistency_all.py
@@ -3,7 +3,10 @@ if __name__ == '__main__':
     import shutil, os, sys
     from tasks import check_consistency
 
-    ignores = ["theorems", "generated", "output","consistency"]
+    #ignores = ["theorems", "generated", "output","consistency"]
+    ignores = ["theorems", "generated", "output"]
+    #necessary = "_nontrivial"
+    necessary = false
     
     good = 0
     bad = 0
@@ -13,7 +16,10 @@ if __name__ == '__main__':
             pass
         else:
             for file in files:
+                print file
                 if file.endswith('.clif'):
+                    if necessary and necessary not in file:
+                        pass
                     filename = os.path.join(dir.replace('qs\\',''), file)
 #                    print filename
                     result = check_consistency.consistent(filename, options=['-simple'])
