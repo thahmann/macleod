f6d843de530e2c6f357e73a79060ab9a442b1cf0
Robert Powell <powellrw7@gmail.com>
2014-11-03 12:53:40 -0500

    A little explanation included

--- src/gui/gui_alpa.py
+++ src/gui/gui_alpa.py
@@ -5,38 +5,44 @@ High-level overview of graphical element to be added to Macleod
 
 """
 
-from Tkinter import Tk, BOTH
-from ttk import Frame, Button, Style
-
-
-class Example(Frame):
-  
-    def __init__(self, parent):
-        Frame.__init__(self, parent)   
-         
-        self.parent = parent
-        
-        self.initUI()
-        
-    def initUI(self):
-      
-        self.parent.title("Quit button")
-        self.style = Style()
-        self.style.theme_use("default")
-
-        self.pack(fill=BOTH, expand=1)
-
-        quitButton = Button(self, text="Quit",
-            command=self.quit)
-        quitButton.place(x=50, y=50)
-
+from Tkinter import *
+import ttk
 
 def main():
   
+    # Toplevel GUI element
     root = Tk()
-    root.geometry("250x150+300+300")
-    app = Example(root)
-    root.mainloop()  
+
+    # Use the ttk frames to hold content for compatibility
+    content = ttk.Frame(root)
+    frame = ttk.Frame(content, borderwidth=5, relief="sunken", height=500, width=500)
+
+    # Labels work like this
+    new_label = ttk.Label(content, text="A Label")
+
+    # Entry field work like this
+    new_entry = ttk.Entry(content)
+
+    # Variables used in frame work like this
+    var_one = BooleanVar() # For checkboxes
+    var_one.set(True)
+    var_two = StringVar() # For entries
+
+    # Assign those variables to elements in gui
+    checkbox_one = ttk.Checkbutton(content, text="First", variable=var_one, onvalue=True)
+    ok_button = ttk.Button(content, text="Ok")
+
+    # Grid the variables on the gui like so
+    content.grid(column=0, row=0)
+    frame.grid(column=0, row=0, columnspan=5, rowspan=5)
+
+    new_label.grid(column=1, row=1)
+    new_entry.grid(column=2, row=2)
+
+    checkbox_one.grid(column=3, row=3)
+    ok_button.grid(column=4, row=4)
+
+    root.mainloop()
 
 
 if __name__ == '__main__':
