6b9515e5c7ee130f7689fea813b6e6b29af562c2
Nathaniel Swan <nathanieljswan@gmail.com>
2015-02-04 15:40:34 -0500

    added aesthetics, added path bar, zoom buttons that don't work yet, and corrected file selection bug

--- gui/gui_alpha.py
+++ gui/gui_alpha.py
@@ -9,7 +9,6 @@ import sys
 import tkFileDialog
 sys.path.append("../tasks")
 
-from visualizer import *
 from Arborist import *
 from check_consistency import *
 from Tkinter import *
@@ -28,6 +27,8 @@ class GUI(Frame):
         self.parent = parent
         
         # defining options for opening a directory
+        self.selected_file = False
+        self.selected_folder = False
         self.dir_opt = self.options = {}
         self.options['initialdir'] = '.'
         self.options['mustexist'] = False
@@ -35,13 +36,18 @@ class GUI(Frame):
         self.options['title'] = 'This is a title'
         self.load_window()
         self.parent.title("Macleod!")
+        self.scale = 1
+        
         
         # define some state variables
 
     def consistency(self, canvas):
         """ Run a hardcoded consistent() """
 
-        derp, clif = consistent("codi/codi_down")
+
+		#change this later to catch a folder and file, not just file
+		
+        derp, clif = consistent(self.selected_file)
         self.arborist = VisualArborist(canvas)
         self.arborist.gather_nodes(clif)
         self.arborist.grow_tree()
@@ -49,8 +55,14 @@ class GUI(Frame):
         self.arborist.weight_tree()
         self.arborist.layout_tree()
         self.arborist.draw_tree()
-        self.merp.set(self.arborist.selected_node.name)
 
+    
+    def zoom(self, io):
+        """ Zoom into and out of the tree """
+        self.scale+=io*.1
+        self.canvas.scale("all",0,0,self.scale,self.scale)
+		
+		    
     def load_window(self):
         """ Setup the with placeholders for stuff """
 
@@ -59,19 +71,8 @@ class GUI(Frame):
 
         right_pane = Frame(self, relief=RAISED, borderwidth=1, width=400, height=500)
         
-        label_1 = Label(right_pane, text="Details").pack(fill=BOTH)
+        label_1 = Label(right_pane, text="Details").pack(fill=BOTH)     
 
-        #TODO remove this later
-        self.merp = StringVar()
-        self.label_2 = Label(right_pane, text=self.merp).pack(fill=BOTH)
-                
-        self.openmenu = StringVar(right_pane)
-        self.openmenu.set("Open...")
-        
-        openFiles = OptionMenu(right_pane, self.openmenu, "File...", "Folder...",command=self.getOption).pack(side=TOP)
-        
-        right_pane.grid(row=1, column=2, rowspan=2, sticky=E+W+S+N)
-        
         """ Create a notebook. This holds the tabs """
         tabs = ttk.Notebook(self, name='the cooliest stuff')
         tabs.grid(row=1, column=1, rowspan=2, stick=E+W+S+N)
@@ -84,47 +85,78 @@ class GUI(Frame):
 
         top_pane = Frame(first_tab, borderwidth=1, relief=SUNKEN)
         
-        canvas = Canvas(top_pane, width=700, height=500)
-        canvas.pack(fill=BOTH)
+        self.canvas = Canvas(top_pane, width=950, height=550)
+        self.canvas.pack(fill=BOTH)
         top_pane.pack(fill=BOTH, expand=1)
+        
+        #ccatch folder again stuff ya
+        """ Footer pane for top level frame """
+        path_pane = Frame(self, borderwidth=1, relief=RAISED)
+        self.selected_path = StringVar()
+        self.selected_label = Label(path_pane, textvariable=self.selected_path)
+        self.selected_path.set("")
+        path_pane.grid(row=3, column=1, columnspan=2, sticky=E+W+S+N)
         
         """ Bottom pane for the first tab """
         bottom_pane = Frame(first_tab, borderwidth=1, relief=SUNKEN)
         
+
+
+        """ Button + Button Button - Button = Pants """
+        bPlus = Button(bottom_pane, text=" + ", \
+                command=lambda: self.zoom(1)).pack(side=RIGHT)
+        bMinus = Button(bottom_pane, text=" - ", \
+                command=lambda: self.zoom(-1)).pack(side=RIGHT)
+        
+        
         """ Creating the buttons for the first tab """
         button_consist = Button(bottom_pane, text="Check Consistency", \
-                command=lambda: self.consistency(canvas))
+                command=lambda: self.consistency(self.canvas))
         button_other = Button(bottom_pane, text="Other Thing")
         
+
+        
+        self.default_dropdown_text = StringVar()
+        self.default_dropdown_text.set("Select File(s)...")
+        
+        
+        openFiles = OptionMenu(bottom_pane, self.default_dropdown_text, "File...", "Folder...",command=self.getOption)
+        
+        right_pane.grid(row=1, column=2, rowspan=2, sticky=E+W+S+N)
+        
         """ Pack the buttons in the bottom pane """
+        openFiles.pack(side=LEFT)
         button_consist.pack(side=LEFT)
         button_other.pack(side=LEFT)
+        self.selected_label.pack(side=LEFT)
         bottom_pane.pack(fill=BOTH, expand=1)
+        
 
         """ Add a second tab in the tabs frame """
         second_tab = Frame(tabs)
         second_tab.pack(fill=BOTH)
         tabs.add(second_tab, text="Summary")
-
         self.pack(fill=BOTH)
+        
+      
 
     def getOption(self,event):
         """ Determine what to do with the selected option """
-        
-        if (self.openmenu.get() == "File..."):
+        if (self.default_dropdown_text.get() == "File..."):
             self.askopenfilename()
-        elif (self.openmenu.get() == "Folder..."):
+        elif (self.default_dropdown_text.get() == "Folder..."):
             self.askdirectory()
     
     def askopenfilename(self):
         """ Returns a selected directory name """
     
-        print tkFileDialog.askopenfilename()
+        self.selected_file = tkFileDialog.askopenfilename()
+        self.selected_path.set("Path:\t"+self.selected_file)
         
     def askdirectory(self):
         """ Returns a selected directory name """
     
-        print tkFileDialog.askdirectory()
+        self.selected_folder = tkFileDialog.askdirectory()
 
 
 
@@ -134,7 +166,8 @@ def main():
     """ Create a new GUI object """
 
     root = Tk()
-    root.geometry("1080x600")
+    root.geometry()
+    root.resizable(0,0)
     app = GUI(root)
     root.mainloop()
 
