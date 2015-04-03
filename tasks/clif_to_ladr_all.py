5ea5788395b8799e51951a092207c5b7f19d6bcc
Nathaniel Swan <nathanieljswan@gmail.com>
2015-04-03 14:59:36 -0500

    fixing this files to be cross platform, and way less redundant

--- tasks/clif_to_ladr_all.py
+++ tasks/clif_to_ladr_all.py
@@ -4,36 +4,15 @@ import os, sys
 from src.ClifModuleSet import ClifModuleSet
 from tasks import clif_to_ladr
 
-def ladr_all(folder):
-    filemgt.start_logging()
-    ending = filemgt.read_config('cl','ending')
-    tempfolder = filemgt.read_config('converters', 'tempfolder')
-    ignores = [tempfolder]
-    for directory, subdirs, files in os.walk(folder):
-        if any(ignore in directory for ignore in ignores):
-            pass
-        else:
-            for single_file in files:
-                if single_file.endswith(ending):
-                    print single_file
-                    filename = os.path.join(directory, single_file)
-                    print filename
-                    m = ClifModuleSet(filename)
-                    clif_to_ladr.ladr(filename, m)
 
-
+#global variables
+filemgt.start_logging()
+tempfolder = filemgt.read_config('converters', 'tempfolder')
+ignores = [tempfolder]
+ending = filemgt.read_config('cl','ending')
+licence.print_terms()
 
 if __name__ == '__main__':
-
-    filemgt.start_logging()
-    tempfolder = filemgt.read_config('converters', 'tempfolder')
-    
-    ignores = [tempfolder]
-    
-    ending = filemgt.read_config('cl','ending')
-
-    licence.print_terms()
-    # global variables
     options = sys.argv
     options.reverse()
     options.pop()
@@ -49,3 +28,20 @@ if __name__ == '__main__':
                     print filename
                     m = ClifModuleSet(filename)
                     clif_to_ladr.ladr(filename, m, options)
+
+
+def ladr_all(folder):
+    filemgt.start_logging()
+    ending = filemgt.read_config('cl','ending')
+    tempfolder = filemgt.read_config('converters', 'tempfolder')
+    ignores = [tempfolder]
+    for directory, subdirs, files in os.walk(folder):
+        if any(ignore in directory for ignore in ignores):
+            pass
+        else:
+            for single_file in files:
+                if single_file.endswith(ending):
+                    filename = os.path.join(directory, single_file)
+                    print filename
+                    m = ClifModuleSet(filename)
+                    clif_to_ladr.ladr(filename, m)
