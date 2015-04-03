6b9f33f5acb9d20d9870ed79c0d6213787ae6e7e
Nathaniel Swan <nathanieljswan@gmail.com>
2015-04-03 00:14:46 -0400

    added method to call directly from gui_alpha.py. Still need to figure out compatability for some issues for windows. The way the file is written does some weird stuff with paths

--- tasks/clif_to_ladr_all.py
+++ tasks/clif_to_ladr_all.py
@@ -1,9 +1,30 @@
 from src import filemgt
 from tasks import *
+import os, sys
+from src.ClifModuleSet import ClifModuleSet
+from tasks import clif_to_ladr
+
+def ladr_all(folder):
+    print "HOLY SHIT MAN"
+    filemgt.start_logging()
+    ending = filemgt.read_config('cl','ending')
+    tempfolder = filemgt.read_config('converters', 'tempfolder')
+    ignores = [tempfolder]
+    for directory, subdirs, files in os.walk(folder):
+        if any(ignore in directory for ignore in ignores):
+            pass
+        else:
+            for single_file in files:
+                if single_file.endswith(ending):
+                    print single_file
+                    filename = os.path.join(directory, single_file)
+                    print filename
+                    m = ClifModuleSet(filename)
+                    clif_to_ladr.ladr(filename, m)
+
+
 
 if __name__ == '__main__':
-    import os, sys
-    from tasks import clif_to_ladr
 
     filemgt.start_logging()
     tempfolder = filemgt.read_config('converters', 'tempfolder')
@@ -27,4 +48,5 @@ if __name__ == '__main__':
                 if single_file.endswith(ending):
                     filename = os.path.join(directory.replace('qs\\',''), single_file)
                     print filename
-                    clif_to_ladr.ladr(filename, options)
+                    m = ClifModuleSet(filename)
+                    clif_to_ladr.ladr(filename, m, options)
