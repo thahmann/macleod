a5c681ee0917b54c4e9b2d7618f0d45bb2988787
Nathaniel Swan <nathanieljswan@gmail.com>
2015-04-09 15:22:38 -0400

    forcing a merge conflict

--- gui/gui_alpha.py
+++ gui/gui_alpha.py
@@ -36,7 +36,7 @@ imgdir = os.path.join(os.path.dirname(__file__), 'img')
 def btn_press(event):
     """ Notebook tab close action """
 
-    x, y, widget = event.x, event.y, event.widget
+    previouslyX, previouslyY, widget = event.x, event.y, event.widget
     elem = widget.identify(x, y)
     index = widget.index("@%d,%d" % (x, y))
 
