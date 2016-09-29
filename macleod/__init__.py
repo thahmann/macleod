d6e8d30d32a6dc78d1fc189c0f60546a602ab05e
Robert Powell <powellrw7@gmail.com>
2016-09-28 20:27:15 -0400

    Restructuring for sensible imports

---
+++ macleod/__init__.py
@@ -0,0 +1,12 @@
+from .ClifModule import *
+from .ClifModuleSet import *
+from .ClifLemmaSet import *
+from .ReasonerSet import *
+from .Reasoner import *
+from .clif import *
+from .commands import *
+from .ladr import *
+from .filemgt import *
+from .process import *
+
+__all__ = ["filemgt", "commands", "clif", "ladr", "process", "ReasonerSet", "Reasoner", "ClifModule", "ClifModuleSet", "ClifLemmaSet"]
