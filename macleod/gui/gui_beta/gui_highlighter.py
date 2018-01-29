927b13af3b78472d66130db300f7ff3416730814
evanmrsampson <evanmrsampson@gmail.com>
2018-01-29 12:58:52 -0500

    Add settings functionality for highlighting Add [gui] section to settings file

--- macleod/gui/gui_beta/gui_highlighter.py
+++ macleod/gui/gui_beta/gui_highlighter.py
@@ -1,14 +1,10 @@
 from PyQt5.Qt import Qt
-from PyQt5.QtCore import QRegularExpression, QRegularExpressionMatchIterator
-from PyQt5.QtGui import QSyntaxHighlighter, QTextCharFormat
+from PyQt5.QtCore import QRegularExpression
+from PyQt5.QtGui import QSyntaxHighlighter, QTextCharFormat, QColor
+import macleod.Filemgt as filemgt
+import configparser
 
-COLOR_EQUALS = Qt.red
-COLOR_PRED = Qt.cyan
-COLOR_FUNC = Qt.magenta
-COLOR_CONNECTIVE = Qt.green
-COLOR_NOT = Qt.gray
-COLOR_QUANTIFIER = Qt.blue
-COLOR_PARENTHESES = Qt.yellow
+COLOR_HIGHLIGHT_SYMBOL = Qt.yellow
 
 
 class CLIFSyntaxHighlighter(QSyntaxHighlighter):
@@ -28,32 +24,32 @@ class CLIFSyntaxHighlighter(QSyntaxHighlighter):
 
         # real colors for functions and predicates
         self.format_predicate = QTextCharFormat()
-        self.format_predicate.setForeground(COLOR_PRED)
+        self.format_predicate.setForeground(get_color("color_predicate"))
         self.format_function = QTextCharFormat()
-        self.format_function.setForeground(COLOR_FUNC)
+        self.format_function.setForeground(get_color("color_function"))
 
         format_equals = QTextCharFormat()
-        format_equals.setForeground(COLOR_EQUALS)
+        format_equals.setForeground(get_color("color_equals"))
         self.rules.append((QRegularExpression("="), 0, format_equals))
 
         format_connective = QTextCharFormat()
-        format_connective.setForeground(COLOR_CONNECTIVE)
+        format_connective.setForeground(get_color("color_connective"))
         connectives = ["AND", "OR", "IF", "IFF"]
         for c in connectives:
             self.rules.append((QRegularExpression("\(" + c, ignore_case), 1, format_connective))
 
         format_not = QTextCharFormat()
-        format_not.setForeground(COLOR_NOT)
+        format_not.setForeground(get_color("color_not"))
         self.rules.append((QRegularExpression("NOT", ignore_case), 0, format_not))
 
         format_quantifier = QTextCharFormat()
-        format_quantifier.setForeground(COLOR_QUANTIFIER)
+        format_quantifier.setForeground(get_color("color_quantifier"))
         quantifiers = ["EXISTS", "FORALL"]
         for q in quantifiers:
             self.rules.append((QRegularExpression("\(" + q, ignore_case), 1, format_quantifier))
 
         format_parentheses = QTextCharFormat()
-        format_parentheses.setForeground(COLOR_PARENTHESES)
+        format_parentheses.setForeground(get_color("color_parentheses"))
         parentheses = ["\(", "\)"]
         for p in parentheses:
             self.rules.append((QRegularExpression(p), 0, format_parentheses))
@@ -79,3 +75,21 @@ class CLIFSyntaxHighlighter(QSyntaxHighlighter):
                             break
                 else:
                     self.setFormat(match.capturedStart() + index, match.capturedLength() - index, format)
+
+    def highlightSymbol(self, symbolName):
+        exp = QRegularExpression(symbolName)
+        highlightFormat = QTextCharFormat()
+        highlightFormat.setBackground(COLOR_HIGHLIGHT_SYMBOL)
+        while self.currentBlock().isValid():
+            iterator = exp.globalMatch(self.currentBlock().text())
+            while iterator.hasNext():
+                match = iterator.next()
+                self.setFormat(match.capturedStart(), match.capturedLength, highlightFormat)
+            self.currentBlock().next()
+
+
+def get_color(setting_name):
+    try:
+        return QColor(filemgt.read_config("gui", setting_name, filemgt.config_file))
+    except configparser.NoOptionError:
+        return QColor()
--- macleod/gui/gui_beta/gui_settings.py
+++ macleod/gui/gui_beta/gui_settings.py
@@ -1,11 +1,13 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 import macleod.Filemgt as filemgt
+import configparser
 import os
 
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 400
+
 
 class MacleodSettings(QDialog):
     def __init__(self, parent=None):
@@ -19,11 +21,20 @@ class MacleodSettings(QDialog):
         self.setSizePolicy(sp)
 
         # root directory settings
-        root_dir_label = QLabel()
-        root_dir_label.setText("Project Root Directory")
+        root_dir_label = QLabel("Project Root Directory")
 
         self.root_dir_edit = QLineEdit()
         self.root_dir_edit.setText(root_dir_path)
+
+        # color settings
+        self._color_equals = self._create_color_button("color_equals")
+        self._color_predicate = self._create_color_button("color_predicate")
+        self._color_function = self._create_color_button("color_function")
+        self._color_connective = self._create_color_button("color_connective")
+        self._color_not = self._create_color_button("color_not")
+        self._color_quantifier = self._create_color_button("color_quantifier")
+        self._color_parentheses = self._create_color_button("color_parentheses")
+        self._color_find = self._create_color_button("color_find")
 
         # exit buttons
         exit_buttons = QDialogButtonBox(self)
@@ -38,6 +49,15 @@ class MacleodSettings(QDialog):
         main_layout.addWidget(tab_controller)
 
         highlighter_layout = QFormLayout(self)
+        highlighter_layout.addRow("Equals: ", self._color_equals)
+        highlighter_layout.addRow("Predicates: ", self._color_predicate)
+        highlighter_layout.addRow("Functions: ", self._color_function)
+        highlighter_layout.addRow("Connectives: ", self._color_connective)
+        highlighter_layout.addRow("Negation: ", self._color_not)
+        highlighter_layout.addRow("Quantifiers: ", self._color_quantifier)
+        highlighter_layout.addRow("Parentheses: ", self._color_parentheses)
+        highlighter_layout.addRow("Find (Highlight): ", self._color_find)
+
         parser_layout = QFormLayout(self)
         parser_layout.addWidget(root_dir_label)
         parser_layout.addWidget(self.root_dir_edit)
@@ -51,9 +71,92 @@ class MacleodSettings(QDialog):
         self.setLayout(main_layout)
 
     def ok_event(self):
+        if self._check_and_update_root_path():
+            self._update_colors()
+            self.close()
+
+    def _create_color_button(self, name):
+        '''Generate a color button for a particular setting, returns the button.'''
+        new_button = QColorButton(name, self)
+        try :
+            new_button.setColor(filemgt.read_config("gui", name))
+        except configparser.NoOptionError:
+            pass
+
+        # This will be a blank color if the setting doesn't exist
+        return new_button
+
+    def _check_and_update_root_path(self):
+        '''Attempt to update in the conf file, returns False if the path is invalid'''
         if not os.path.isdir(self.root_dir_edit.text()):
             self.root_dir_edit.clear()
-            return
-
+            self.root_dir_edit.setPlaceholderText("Invalid Path")
+            return False
         filemgt.edit_config('system', 'path', self.root_dir_edit.text(), filemgt.config_file)
-        self.close()
+        return True
+
+    def _update_colors(self):
+        self._color_equals.update_color()
+        self._color_predicate.update_color()
+        self._color_function.update_color()
+        self._color_connective.update_color()
+        self._color_not.update_color()
+        self._color_quantifier.update_color()
+        self._color_parentheses.update_color()
+        self._color_find.update_color()
+
+
+# taken from http://pyqt.sourceforge.net/Docs/PyQt5/signals_slots.html
+class QColorButton(QPushButton):
+    '''
+    Custom Qt Widget to show a chosen color.
+
+    Left-clicking the button shows the color-chooser, while
+    right-clicking resets the color to None (no-color).
+    '''
+
+    colorChanged = pyqtSignal()
+
+    def __init__(self, setting_name="", *args, **kwargs):
+        super(QColorButton, self).__init__(*args, **kwargs)
+
+        self._color = None
+        self._setting_name = setting_name
+        self.setMaximumWidth(32)
+        self.pressed.connect(self.onColorPicker)
+
+    def setColor(self, color):
+        if color != self._color:
+            self._color = color
+            self.colorChanged.emit()
+
+        if self._color:
+            self.setStyleSheet("background-color: %s;" % self._color)
+        else:
+            self.setStyleSheet("")
+
+    def color(self):
+        return self._color
+
+    def onColorPicker(self):
+        '''
+        Show color-picker dialog to select color.
+
+        Qt will use the native dialog by default.
+
+        '''
+        dlg = QColorDialog(self)
+        if self._color:
+            dlg.setCurrentColor(QColor(self._color))
+
+        if dlg.exec_():
+            self.setColor(dlg.currentColor().name())
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.RightButton:
+            self.setColor(None)
+
+        return super(QColorButton, self).mousePressEvent(e)
+
+    def update_color(self):
+        filemgt.edit_config("gui", self._setting_name, self.color(), filemgt.config_file)
