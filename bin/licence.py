de83f91bcd3877828b7e08a41da6ad80588471c1
thahmann <torsten.hahmann@maine.edu>
2019-03-08 13:35:58 -0500

    Update licence.py

--- bin/licence.py
+++ bin/licence.py
@@ -3,7 +3,7 @@
 def print_terms():
 
     print ("""
-        Macleod  Copyright (C) 2011-2013 Torsten Hahmann, University of Toronto
+        Macleod  Copyright (C) 2011-2019 Torsten Hahmann and others
         ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         Macleod comes with ABSOLUTELY NO WARRANTY.
         This is free software, licensed under the GNU General Public (GPL) Licence,
